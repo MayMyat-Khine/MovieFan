4e27b251ea7b5afe5b99e3bb3abecabdab745c25
MayMyat <khinemyat312@gmail.com>
2024-07-18 14:52:12 +0630

    Initial Commit

---
+++ MovieFan/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MovieFan
+//
+//  Created by Khine Myat on 18/07/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MovieFanUITests/MovieFanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieFanUITestsLaunchTests.swift
+//  MovieFanUITests
+//
+//  Created by Khine Myat on 18/07/2024.
+//
+
+import XCTest
+
+final class MovieFanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
